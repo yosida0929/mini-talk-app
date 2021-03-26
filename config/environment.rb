4d9feae97a59187f15fcf50b6eb9ad844a629585
yosida0929 <xqkkr256@gmail.com>
2021-03-26 21:30:39 +0900

    Elastic IP記述

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the Rails application.
 require_relative 'application'
 
-# Initialize the Rails application.
+ActionCable.server.config.disable_request_forgery_protection = true
+  config.action_cable.url = "ws://35.73.13.95/cable" 
+  config.action_cable.allowed_request_origins = ['http://35.73.13.95']
 Rails.application.initialize!
